199660ca7175be3c3f11e73803e7e3f2215ee149
Papatyakiz <skasymowna@gmail.com>
2025-04-02 19:13:29 +0500

    seventh commit

--- database/migrations/2025_03_31_191319_create_products_table.php
+++ database/migrations/2025_03_31_191319_create_products_table.php
@@ -15,7 +15,6 @@ return new class extends Migration
             $table->id();
             $table->foreignId('restaurant_id')->constrained()->onDelete('cascade');
             $table->foreignId('category_id')->constrained()->onDelete('cascade');
-            $table->string('name');
             $table->text('description');
             $table->double('price');
             $table->timestamps();
